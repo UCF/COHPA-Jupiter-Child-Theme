0a5bd9820991f3d60387f6509590fc156dba25ea
cohpa <djanosik@ucf.edu>
2015-11-12 10:50:21 -0500

    Modified Loop with Variables
    Added actual variables, and changed the loop sequence to add rows per
    entry.

--- archive-committee.php
+++ archive-committee.php
@@ -15,7 +15,7 @@ get_header(); ?>
 	<div class="mk-main-wrapper-holder">
 		<div id="mk-page-id-<?php echo $post->ID; ?>" class="theme-page-wrapper mk-main-wrapper <?php echo $page_layout; ?>-layout <?php echo $padding; ?> mk-grid vc_row-fluid">
 			<div class="theme-content <?php echo $padding; ?>" itemprop="mainContentOfPage">
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
+				
 										
                         
                         
@@ -34,52 +34,26 @@ get_header(); ?>
 </tr>
 </thead>
 <tbody>
+<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
 <tr>
 <td colspan="3">
 <h2 class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style " style="font-size: 14px; text-align: left; color: #3d3d3d; font-style: inherit; font-weight: bold; padding-top: 0px; padding-bottom: 0px; text-transform: initial; letter-spacing: 0px; margin-bottom: 0px;">Undergraduate &amp; Graduate</h2>
 </td>
 </tr>
 <tr>
-<td><a title="Academic Affairs Committee" href="https://www.cohpa.ucf.edu/committees/academic-affairs-committee/">Academic Affairs Committee</a></td>
-<td>Reviews &amp; approves new programs, program revisions</td>
-<td>Faculty</td>
+<td><a title="<?php the_title(); ?>" href="<?php get_permalink(); ?>"><?php the_title(); ?></a></td>
+<td><?php get_field('charge'); ?></td>
+<td><?php get_field('member_type'); ?></td>
 </tr>
-<tr>
-<td><a title="Divisional Review Committee" href="https://www.cohpa.ucf.edu/committees/divisional-review-committee/">Divisional Review Committee</a></td>
-<td>Reviews &amp; evaluates program quality</td>
-<td>Faculty or Staff</td>
-</tr>
-<tr>
-<td><a title="Student Affairs Committee" href="https://www.cohpa.ucf.edu/committees/student-affairs-committee/">Student Affairs Committee</a></td>
-<td>Reviews grade appeals, academic advising matters</td>
-<td>Faculty</td>
-</tr>
-<tr>
-<td><a title="Student Advisory Council" href="https://www.cohpa.ucf.edu/committees/student-advisory-council/">Student Advisory Council</a></td>
-<td>Advises dean on student needs, views</td>
-<td>Students</td>
-</tr>
+<?php endwhile; ?>
 </tbody>
 </table>
 </div>
 
 	</div></div>
 	</div></div>
-                        
-                        
-                        
-                        
-                        
-                        
-						<?php wp_link_pages( 'before=<div id="mk-page-links">'.__( 'Pages:', 'mk_framework' ).'&after=</div>' ); ?>
-				<?php endwhile; ?>
-						<?php
-						if($mk_options['pages_comments'] == 'true') {
-							if ( comments_open() ) :
-							comments_template( '', true ); 	
-							endif;
-						}
-						?>
+
+
 				</div>
 			
 		<?php if ( $page_layout != 'full' ) get_sidebar(); ?>
