25ad95bddf48ee4522661564e16a5c287a42ae3a
cohpa <djanosik@ucf.edu>
2015-11-12 12:23:06 -0500

    new program call

--- archive-committee.php
+++ archive-committee.php
@@ -36,9 +36,10 @@ get_header(); ?>
 <tbody>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
 <?php if( get_field('program') ): ?>
+<?php $terms = get_field('program');?>
 <tr>
 <td colspan="3">
-<h2 class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style " style="font-size: 14px; text-align: left; color: #3d3d3d; font-style: inherit; font-weight: bold; padding-top: 0px; padding-bottom: 0px; text-transform: initial; letter-spacing: 0px; margin-bottom: 0px;"><?php the_field('program'); ?></h2>
+<h2 class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style " style="font-size: 14px; text-align: left; color: #3d3d3d; font-style: inherit; font-weight: bold; padding-top: 0px; padding-bottom: 0px; text-transform: initial; letter-spacing: 0px; margin-bottom: 0px;"><?php  echo $term->name; ?></h2>
 </td>
 </tr>
 <?php endif; ?>
