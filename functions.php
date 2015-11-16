81ae5bdcf817d8ae1657d23ecc96f3d3469b1c7d
cohpa <djanosik@ucf.edu>
2015-11-16 16:36:23 -0500

    Changed template to stylesheet
    Trying to call Child Theme css file.. not main theme files.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php // ADDED BY DAVID JANOSIK
 	function load_custom_wp_admin_style() {
-        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/custom-admin-style.css', false, '1.0.0' );
+        wp_register_style( 'custom_wp_admin_css', get_stylesheet_directory_uri() . '/custom-admin-style.css', false, '1.0.0' );
         wp_enqueue_style( 'custom_wp_admin_css' );
 		}
 		add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
