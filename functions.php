217e518d0f10c59fcf42ff4799d69db9fb6f11ee
cohpa <djanosik@ucf.edu>
2015-11-16 16:13:39 -0500

    change css directory call

--- functions.php
+++ functions.php
@@ -291,7 +291,7 @@ class Theme
 	
 	// ADDED BY DAVID JANOSIK
 	function load_custom_wp_admin_style() {
-        wp_register_style( 'custom_wp_admin_css', THEME_DIR . '/custom-admin-style.css', false, '1.0.0' );
+        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/custom-admin-style.css', false, '1.0.0' );
         wp_enqueue_style( 'custom_wp_admin_css' );
 		}
 		add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
