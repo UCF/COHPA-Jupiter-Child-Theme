0878022eb12303787a78c32e75da8f7f18c027a5
cohpa <djanosik@ucf.edu>
2015-11-16 16:14:34 -0500

    removed call

--- functions.php
+++ functions.php
@@ -288,12 +288,5 @@ class Theme
         $page = include (THEME_ADMIN . '/admin-panel/masterkey.php');
         new mkOptionGenerator($page['name'], $page['options']);
     }
-	
-	// ADDED BY DAVID JANOSIK
-	function load_custom_wp_admin_style() {
-        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/custom-admin-style.css', false, '1.0.0' );
-        wp_enqueue_style( 'custom_wp_admin_css' );
-		}
-		add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 }
 ?>
