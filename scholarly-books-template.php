4ae876fc35eb673246d439a50b16af57359886c3
cohpa <djanosik@ucf.edu>
2016-01-05 14:27:15 -0500

    test

--- scholarly-books-template.php
+++ scholarly-books-template.php
@@ -22,15 +22,7 @@ get_header(); ?>
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
 						<?php the_content();?>
 						<div class="clearboth"></div>
-						<?php wp_link_pages( 'before=<div id="mk-page-links">'.__( 'Pages:', 'mk_framework' ).'&after=</div>' ); ?>
-				<?php endwhile; ?>
-						<?php
-						if($mk_options['pages_comments'] == 'true') {
-							if ( comments_open() ) :
-							comments_template( '', true ); 	
-							endif;
-						}
-						?>
+
 				
                         
                         
@@ -63,32 +55,7 @@ get_header(); ?>
             </div>
         </div>
         
-        
-        
-        <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid" style="float:left;">
-        	<div class="wpb_column vc_column_container vc_col-sm-3">
-            	<div class="wpb_wrapper">
-                	<div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px">
-                    	<div class="mk-image-inner">
-                        	<a href="#" target="_self" class="mk-image-shortcode-link">
-                            	<img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2015/11/9781611636857_thumb.jpg" />
-                            </a>
-                        </div>
-                        <div class="clearboth"></div>
-                    </div>
-               </div>
-          </div>
-          <div class="wpb_column vc_column_container vc_col-sm-9">
-          	<div class="wpb_wrapper">
-            	<h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c136ea54c1" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#">A Primer on Crime and Delinquency Theory</a></span></h2>
-                <div class="clearboth"></div>
-                <div style="text-align: left;" class="mk-text-block  true">
-                	<p>By <strong>Robert M. Bohm</strong>, professor of criminal justice, and Brenda L. Vogel</p>
-					<p><em>Published in: February 2015</em></p>
-					<div class="clearboth"></div>
-                </div>
-            </div>
-        </div>
+
         
         
         
