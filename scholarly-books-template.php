4b10518613066b37a373b8b9567e86a9b2980a68
cohpa <djanosik@ucf.edu>
2016-01-05 15:59:19 -0500

    inserted PHP values

--- scholarly-books-template.php
+++ scholarly-books-template.php
@@ -30,50 +30,32 @@ get_header(); ?>
 
 
 
+</div></div></div><div id="mk-page-section-568c239d0b628" data-intro-effect="false" class="full-width-568c239d0b628  full-height-false mk-page-section self-hosted mk-blur-parent mk-shortcode  " ><div class="mk-video-color-mask"></div><div class="background-clipper"></div><div class="mk-grid vc_row-fluid page-section-content"><div class="mk-padding-wrapper">
 
-<div class="clearboth"></div></div></div></div><div id="mk-page-section-568c239d0b628" data-intro-effect="false" class="full-width-568c239d0b628  full-height-false mk-page-section self-hosted mk-blur-parent mk-shortcode  " ><div class="mk-video-color-mask"></div><div class="background-clipper"></div><div class="mk-grid vc_row-fluid page-section-content"><div class="mk-padding-wrapper">
 
 
+ <?php
+
+// check if the repeater field has rows of data
+if( have_rows('scholarly_books') ):
+
+ 	// loop through the rows of data
+    while ( have_rows('scholarly_books') ) : the_row();?> 
+   
+
+
+<!-- START OF THE REPEAT SECTION -->
 <div style="float:left;" class="vc_col-sm-6 wpb_column column_container ">
 			
-            <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid vc_custom_1452017887231"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="wpb_wrapper"><div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px"><div class="mk-image-inner"><a href="#" target="_self" class="mk-image-shortcode-link"><img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2015/11/9781611636857_thumb.jpg" /></a></div><div class="clearboth"></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-9"><div class="wpb_wrapper"><h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c239d0d942" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#">A Primer on Crime and Delinquency Theory</a></span></h2><div class="clearboth"></div>	<div style="text-align: left;" class="mk-text-block  true"><p>By <strong>Robert M. Bohm</strong>, professor of criminal justice, and Brenda L. Vogel</p>
-<p><em>Published in: February 2015</em></p>
+            <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid vc_custom_1452017887231"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="wpb_wrapper"><div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px"><div class="mk-image-inner"><a href="#" target="_self" class="mk-image-shortcode-link"><img class="lightbox-false" alt="" title="" src="<?php the_sub_field('book_cover'); ?>" /></a></div><div class="clearboth"></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-9"><div class="wpb_wrapper"><h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c239d0d942" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#"><?php the_sub_field('book_title'); ?></a></span></h2><div class="clearboth"></div>	<div style="text-align: left;" class="mk-text-block  true">
+            <?php the_sub_field('book_citation'); ?>
+<p><em>Published in: <?php the_sub_field('publish_date'); ?></em></p>
 <div class="clearboth"></div></div> </div></div></div>
 <div class="mk-shortcode mk-padding-shortcode" style="height:40px"></div>
 </div>
 
-
-
-
-<div style="float:left;" class="vc_col-sm-6 wpb_column column_container ">
-			
-            <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid vc_custom_1452017887231"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="wpb_wrapper"><div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px"><div class="mk-image-inner"><a href="#" target="_self" class="mk-image-shortcode-link"><img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2015/11/9781611636857_thumb.jpg" /></a></div><div class="clearboth"></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-9"><div class="wpb_wrapper"><h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c239d0d942" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#">A Primer on Crime and Delinquency Theory</a></span></h2><div class="clearboth"></div>	<div style="text-align: left;" class="mk-text-block  true"><p>By <strong>Robert M. Bohm</strong>, professor of criminal justice, and Brenda L. Vogel</p>
-<p><em>Published in: February 2015</em></p>
-<div class="clearboth"></div></div> </div></div></div>
-<div class="mk-shortcode mk-padding-shortcode" style="height:40px"></div>
-</div>
-
-
-
-
-<div style="float:left;" class="vc_col-sm-6 wpb_column column_container ">
-			
-            <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid vc_custom_1452017887231"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="wpb_wrapper"><div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px"><div class="mk-image-inner"><a href="#" target="_self" class="mk-image-shortcode-link"><img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2015/11/9781611636857_thumb.jpg" /></a></div><div class="clearboth"></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-9"><div class="wpb_wrapper"><h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c239d0d942" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#">A Primer on Crime and Delinquency Theory</a></span></h2><div class="clearboth"></div>	<div style="text-align: left;" class="mk-text-block  true"><p>By <strong>Robert M. Bohm</strong>, professor of criminal justice, and Brenda L. Vogel</p>
-<p><em>Published in: February 2015</em></p>
-<div class="clearboth"></div></div> </div></div></div>
-<div class="mk-shortcode mk-padding-shortcode" style="height:40px"></div>
-</div>
-
-
-
-
-<div style="float:left;" class="vc_col-sm-6 wpb_column column_container ">
-			
-            <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid vc_custom_1452017887231"><div class="wpb_column vc_column_container vc_col-sm-3"><div class="wpb_wrapper"><div class="mk-image-shortcode mk-shortcode   align-left mk-animate-element fade-in simple-frame inside-image " style="max-width: 800px; margin-bottom:10px"><div class="mk-image-inner"><a href="#" target="_self" class="mk-image-shortcode-link"><img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2015/11/9781611636857_thumb.jpg" /></a></div><div class="clearboth"></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-9"><div class="wpb_wrapper"><h2 style="font-size: 20px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-568c239d0d942" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style=""><a href="#">A Primer on Crime and Delinquency Theory</a></span></h2><div class="clearboth"></div>	<div style="text-align: left;" class="mk-text-block  true"><p>By <strong>Robert M. Bohm</strong>, professor of criminal justice, and Brenda L. Vogel</p>
-<p><em>Published in: February 2015</em></p>
-<div class="clearboth"></div></div> </div></div></div>
-<div class="mk-shortcode mk-padding-shortcode" style="height:40px"></div>
-</div>
+<!-- END OF THE REPEAT SECTION -->
+<?php endwhile; ?> 
 
 
 
