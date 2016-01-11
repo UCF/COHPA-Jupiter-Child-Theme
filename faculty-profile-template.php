05657501f30b0dc913879b41fb00942d5dfca976
cohpa <djanosik@ucf.edu>
2016-01-11 15:37:17 -0500

    tweak

--- faculty-profile-template.php
+++ faculty-profile-template.php
@@ -35,12 +35,16 @@ $wp_user_search = $wpdb->get_results("SELECT ID, display_name FROM $wpdb->users 
 foreach ( $wp_user_search as $userid ) {
 	$user_id       = (int) $userid->ID;
 	$user_login    = stripslashes($userid->user_login);
-	$display_name  = stripslashes($userid->display_name);
-
-	$return  = '';
-	$return .= "\t" . '<li>'. $display_name .'</li>' . "\n";
-
-	print($return);
+	$display_name  = stripslashes($userid->display_name);?>
+    
+    
+    <li>Hello</li>
+    <?php the_field('upload_headshot'); ?>
+    
+<?php
+	//$return  = '';
+	//$return .= "\t" . '<li>'. $display_name .'</li>' . "\n";
+	//print($return);
 }
 ?>
 
