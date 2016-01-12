6b1d8c76502c6dc3fda0d7b6c0f5ef946abda7f0
cohpa <djanosik@ucf.edu>
2016-01-12 12:06:07 -0500

    new HTML layout

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -18,6 +18,80 @@
 ?>
 <p><?php the_field('upload_headshot', 'user_' . $user_id .''); ?></p>
 
+
+
+
+
+<div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid">
+	<div style="" class="vc_col-sm-3 wpb_column column_container ">
+		<div style="text-align: left;" class="mk-text-block  true">
+        	<p>INSERT SIDEBAR HERE</p>
+			<div class="clearboth"></div>
+        </div> 
+	</div>
+	
+    <div style="" class="vc_col-sm-9 wpb_column column_container ">
+		<div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid">
+        	<div class="wpb_column vc_column_container vc_col-sm-4">
+            	<div class="wpb_wrapper">
+                	<div style="text-align: left;" class="mk-text-block  true">
+                    	<p>INSERT PROFILE PIC</p>
+						<div class="clearboth"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="wpb_column vc_column_container vc_col-sm-8">
+            	<div class="wpb_wrapper">
+                	<div style="text-align: left;" class="mk-text-block  true">
+                    	<p>INSERT GENERAL INFO</p>
+						<div class="clearboth"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="padding: 20px 0 40px;" class="mk-divider mk-shortcode divider_full_width center shadow_line ">
+        	<div class="divider-inner"><span class="divider-shadow-left"></span><span class="divider-shadow-right"></span></div>
+        </div>
+        <div class="clearboth"></div>
+        <div class="wpb_row vc_inner vc_row  vc_row-fluid   attched-false vc_row-fluid">
+        	<div class="wpb_column vc_column_container vc_col-sm-9">
+            	<div class="wpb_wrapper">
+                	<div style="text-align: left;" class="mk-text-block  true">
+                    	<p>INSERT TABBED CONTENT</p>
+						<div class="clearboth"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="wpb_column vc_column_container vc_col-sm-3">
+            	<div class="wpb_wrapper">
+                	<div style="text-align: left;" class="mk-text-block  true">
+                    	<p>INSERT LINKS SECTION</p>
+						<div class="clearboth"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- START PROFIL PIC SECTION -->  
 <div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid">
 	<div style="" class="vc_col-sm-4 wpb_column column_container ">
