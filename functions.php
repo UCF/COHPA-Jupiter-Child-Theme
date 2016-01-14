e023134b43861d2e5d9868f8f873eebf4c5c1da7
cohpa <djanosik@ucf.edu>
2016-01-14 17:11:12 -0500

    tweak

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 		}
 		add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
-add_image_size( 'faculty_directory', 100, 200, true ); // Hard crop center center
+add_image_size( 'faculty', 100, 200, true ); // Hard crop center center
 
 add_action('admin_head', 'admin_styles');
 function admin_styles() {
