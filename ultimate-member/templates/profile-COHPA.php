0bcc7497216abe99cf260937ac45d5252047e2d6
cohpa <djanosik@ucf.edu>
2016-01-14 10:19:41 -0500

    tweak

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -372,14 +372,14 @@
 							the_field('youtube_url', 'user_' . $user_id .'');
 							echo '" target="_blank">YouTube</a></div>';
 						}
-					   echo '</div></div><div id="facultytabContainer"></div><div id="facultytabpage_2" class="facultytabpage"></div><h2></h2><div class="clearboth"></div></div>';
+					   echo '<p>&nbsp;</p></div></div><div id="facultytabContainer"></div><div id="facultytabpage_2" class="facultytabpage"></div><h2></h2><div class="clearboth"></div></div>';
 					}?>
                     
                     
                     
                     <?php
 					   if (have_rows('affiliations', 'user_' . $user_id .'') ) { 
-						echo '<div style=" margin-bottom:0px;text-align: left;" class="mk-text-block  "><h3 class="mk-shortcode mk-fancy-title pattern-style mk-shortcode-heading pattern-false"><span >Affiliations</span></h3><div id="cohpa-directoryDetail-rightcolumn"><div id="cohpa-directoryDetail-rightcolumn-GRAY">';?>
+						echo '<div style=" margin-bottom:0px;text-align: left;" class="mk-text-block  "><h3 class="mk-shortcode mk-fancy-title pattern-style mk-shortcode-heading pattern-false"><span >Affiliations</span></h3>';?>
 							<?php while (have_rows('affiliations', 'user_' . $user_id .'') ): the_row(); ?>
                                 <div id="directoryProfile-link"><i style="color:#666;margin:4px;4px;" class="mk-icon-external-link  mk-size-small"></i>
                                         <a href="<?php the_sub_field('aff_url', 'user_' . $user_id .''); ?>" title="<?php the_sub_field('aff_name', 'user_' . $user_id .''); ?>" target="_blank"><?php the_sub_field('aff_name', 'user_' . $user_id .''); ?></a>
