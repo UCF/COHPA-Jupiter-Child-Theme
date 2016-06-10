0030ecba6c0be8cdb0596c5080ea9f37a2c1f940
cohpa <djanosik@ucf.edu>
2016-06-10 14:10:53 -0400

    restructure

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -304,36 +304,22 @@ else { ?>
 								if(get_field('research_info', 'user_' . $user_id .'')) { 
 									echo '<div id="1423603736-2-45" class="mk-tabs-pane"><div class="title-mobile"><i class=""></i>Research</div>	<div style=" margin-bottom:0px;text-align: left;" class="mk-text-block  ">';
 									the_field('research_info', 'user_' . $user_id .'');
-									
-									
-									echo '<h4>Research Interests</h4>';
-									
-									
-									the_field('research_interests', 'user_' . $user_id .'', $term);
-									
-                                    echo '<h4>TRY LOOP</h4>';
-									
 									?>
                                     
 									<?php 
-
 									$termswer = get_field('research_interests', 'user_' . $user_id .'');
-									
-									if( $termswer ): ?>
-									
+									if( $termswer ): 
+									echo '<h4>Research Interests</h4>';
+									?>
 										<ul>
-									
 										<?php foreach( $termswer as $term ): ?>
-									
-											<h2><?php echo $term->name; ?></h2>
-											<p><?php echo $term->description; ?></p>
-									
+											<li><?php echo $term->name; ?></li>
 										<?php endforeach; ?>
-									
 										</ul>
-									
 									<?php endif; ?>
-									
+
+
+
 								<?php 	
 									
 									echo '<div class="clearboth"></div></div><div class="clearboth"></div></div>';
