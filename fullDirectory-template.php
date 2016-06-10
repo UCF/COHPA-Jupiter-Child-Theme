7ae56d2a0ab7443798b0b0202ba832fbd123464e
cohpa <djanosik@ucf.edu>
2016-06-10 15:03:08 -0400

    New Template Attempt

---
+++ fullDirectory-template.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Template Name: Directory Listing
+*/
+
+
+global $post,
+$mk_options;
+$page_layout = get_post_meta( $post->ID, '_layout', true );
+$padding = get_post_meta( $post->ID, '_padding', true );
+
+
+if ( empty( $page_layout ) ) {
+	$page_layout = 'full';
+}
+$padding = ($padding == 'true') ? 'no-padding' : '';
+
+get_header(); ?>
+<div id="theme-page" <?php echo get_schema_markup('main'); ?>>
+	<div class="mk-main-wrapper-holder">
+		<div id="mk-page-id-<?php echo $post->ID; ?>" class="theme-page-wrapper mk-main-wrapper <?php echo $page_layout; ?>-layout <?php echo $padding; ?> mk-grid vc_row-fluid">
+			<div class="theme-content <?php echo $padding; ?>" itemprop="mainContentOfPage">
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
+						<?php the_content();?>
+                  <?php endwhile; ?>
+                <?php wp_reset_query(); ?>       
+                        
+                        
+<!-- START THE CUSTOM SECTION -->
+
+<div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid">
+	
+<?php
+$args = array(
+	.
+	.
+	.
+);
+
+// The Query
+$user_query = new WP_User_Query( $args );
+
+// User Loop
+if ( ! empty( $user_query->results ) ) {
+	foreach ( $user_query->results as $user ) {
+		echo '<p>' . $user->display_name . '</p>';
+	}
+} else {
+	echo 'No users found.';
+}
+?>
+    
+</div>
+						
+<!-- END THE CUSTOM SECTION -->
+                        
+                        
+					
+                    
+                    
+                    <div class="clearboth"></div>
+					<?php wp_link_pages( 'before=<div id="mk-page-links">'.__( 'Pages:', 'mk_framework' ).'&after=</div>' ); ?>
+						<?php
+						if($mk_options['pages_comments'] == 'true') {
+							if ( comments_open() ) :
+							comments_template( '', true ); 	
+							endif;
+						}
+						?>
+				</div>
+			
+		<?php if ( $page_layout != 'full' ) get_sidebar(); ?>
+		<div class="clearboth"></div>
+		</div>
+		<div class="clearboth"></div>
+	</div>	
+</div>
+<?php get_footer(); ?>
