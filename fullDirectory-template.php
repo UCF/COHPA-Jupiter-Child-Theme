6e7d0ff828c137ccb494d99aee39b0c8c27f4ed7
cohpa <djanosik@ucf.edu>
2016-06-16 15:30:06 -0400

    in form

--- fullDirectory-template.php
+++ fullDirectory-template.php
@@ -29,126 +29,6 @@ get_header(); ?>
                         
 <!-- START THE CUSTOM SECTION -->
 
-<div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid vc_custom_1455896967960" style="padding-bottom:20px; margin-bottom:20px; border-bottom:1px solid #ddd;">
-	<div style="" class="vc_col-sm-2 wpb_column column_container ">
-			<div class="mk-image-shortcode mk-shortcode   align-left single_line-frame inside-image " style="max-width: 600px; margin-bottom:10px">
-            	<div class="mk-image-inner">
-                	<a href="#" target="_self" class="mk-image-shortcode-link">
-                    	<img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/bfi_thumb/david-headshot-mkvq9zv6svldlz02s8jfvwa3h52dffhj4jne5yzcrc.jpg" />
-                     </a>
-                </div>
-                <div class="clearboth"></div>
-            </div>
-	</div>
-	<div style="" class="vc_col-sm-6 wpb_column column_container ">
-        <h2 style="font-size: 25px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e076fc" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style directoryNameFixer">
-            <span style="">
-                <a title="David Janosik" href="https://www.cohpa.ucf.edu/directory/david-janosik/" target="_parent">David Janosik</a>
-            </span>
-        </h2>
-        <div class="clearboth"></div>
-        <div style=" margin-bottom:10px;text-align: left;" class="mk-text-block  true">
-            <div id="cohpa-directory-name">
-                <strong>Web Applications Developer</strong><br />
-                <span class="color-lightgray">Dean&#8217;s Office</span><br />
-                <span class="color-lightgray">Phone: </span>407-823-5884<br />
-                <span class="color-lightgray">Email: </span>
-                <a title="Contact David Janosik" href="mailto:djanosik@ucf.edu">djanosik@ucf.edu</a>
-            </div>
-            <div class="clearboth"></div>
-        </div>
-        <div style="text-align: left;" class="mk-text-block  true">
-            <div id="cohpa-directory-name"></div>
-            <div>Location: <a class="color-darkGold" title="Map to HPA I" href="http://map.ucf.edu/locations/80/health-public-affairs-i/" target="_blank">HPA I</a> Room: 341</div>
-            <div class="clearboth"></div>
-        </div> 
-    </div>
-    <div style="" class="vc_col-sm-4 wpb_column column_container ">
-        <h2 style="font-size: 16px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:normal;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e07f2d" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style ">
-            <span style="">Additional Info</span>
-        </h2>
-        <div class="clearboth"></div>
-        <div style="text-align: left;" class="mk-text-block  true">
-            <p>
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-file-pdf-o mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Curriculum Vitae
-            </a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-external-link-square mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Personal Website</a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-facebook-square mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Facebook</a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-moon-linkedin mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Linkedin</a></p>
-            <div class="clearboth"></div>
-        </div> 
-    </div>
-</div>
-
-
-
-
-<div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid vc_custom_1455896967960" style="padding-bottom:20px; margin-bottom:20px; border-bottom:1px solid #ddd;">
-	<div style="" class="vc_col-sm-2 wpb_column column_container ">
-			<div class="mk-image-shortcode mk-shortcode   align-left single_line-frame inside-image " style="max-width: 600px; margin-bottom:10px">
-            	<div class="mk-image-inner">
-                	<a href="#" target="_self" class="mk-image-shortcode-link">
-                    	<img class="lightbox-false" alt="" title="" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/bfi_thumb/david-headshot-mkvq9zv6svldlz02s8jfvwa3h52dffhj4jne5yzcrc.jpg" />
-                     </a>
-                </div>
-                <div class="clearboth"></div>
-            </div>
-	</div>
-	<div style="" class="vc_col-sm-6 wpb_column column_container ">
-        <h2 style="font-size: 25px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e076fc" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style directoryNameFixer">
-            <span style="">
-                <a title="David Janosik" href="https://www.cohpa.ucf.edu/directory/david-janosik/" target="_parent">David Janosik</a>
-            </span>
-        </h2>
-        <div class="clearboth"></div>
-        <div style=" margin-bottom:10px;text-align: left;" class="mk-text-block  true">
-            <div id="cohpa-directory-name">
-                <strong>Web Applications Developer</strong><br />
-                <span class="color-lightgray">Dean&#8217;s Office</span><br />
-                <span class="color-lightgray">Phone: </span>407-823-5884<br />
-                <span class="color-lightgray">Email: </span>
-                <a title="Contact David Janosik" href="mailto:djanosik@ucf.edu">djanosik@ucf.edu</a>
-            </div>
-            <div class="clearboth"></div>
-        </div>
-        <div style="text-align: left;" class="mk-text-block  true">
-            <div id="cohpa-directory-name"></div>
-            <div>Location: <a class="color-darkGold" title="Map to HPA I" href="http://map.ucf.edu/locations/80/health-public-affairs-i/" target="_blank">HPA I</a> Room: 341</div>
-            <div class="clearboth"></div>
-        </div> 
-    </div>
-    <div style="" class="vc_col-sm-4 wpb_column column_container ">
-        <h2 style="font-size: 16px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:normal;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e07f2d" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style ">
-            <span style="">Additional Info</span>
-        </h2>
-        <div class="clearboth"></div>
-        <div style="text-align: left;" class="mk-text-block  true">
-            <p>
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-file-pdf-o mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Curriculum Vitae
-            </a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-external-link-square mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Personal Website</a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-icon-facebook-square mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Facebook</a><br />
-            <a title="Curriculum Vitae for David Janosik" href="https://www.cohpa.ucf.edu/media/496089/janosik-2013.pdf" target="_parent">
-                <i class="mk-moon-linkedin mk-size-small" style="color: #c1c1c1; margin: 4px 4px;"></i> Linkedin</a></p>
-            <div class="clearboth"></div>
-        </div> 
-    </div>
-</div>
-
- <!-- END -->   
-
-
-
-
-
-	
 <?php
 $args = array(
 	'order' => 'ASC',
@@ -172,58 +52,56 @@ if ( ! empty( $user_query->results ) ) {
 		$width = $image['sizes'][ $size . '-width' ];
 		$height = $image['sizes'][ $size . '-height' ];
 ?>  
-		
-		
-		<?php
-			if( $image ) { ?>
-				<a href="<?php echo um_user_profile_url(); ?>" title="<?php echo '' . $user->display_name . ''; ?>">
-                <img src="<?php echo $thumb; ?>" alt="<?php echo '' . $user->display_name . ''; ?>" title="<?php echo '' . $user->display_name . ''; ?>" width="150px" />
-                </a>
-				 
-			<?php }
-			else { ?> 
-				<img class="lightbox-true" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" src="/wp-content/uploads/2016/01/defaul-avatar_0.jpg" itemprop="image"  width="150px" />
-			<?php } ?>  
-		
-<?php		
-		echo '<h2><a href="';
-		echo um_user_profile_url();
-		echo '" title="';
-		echo '' . $user->display_name . '';
-		echo '">';
-		echo '' . $user->display_name . '';
-		echo '</a></h2>';
-		//echo '<p>' . $user->phone_number . '</p>';
-		//echo '<p>Hello 7</p>';
-		//echo '<p>' . $user->id . '</p>';
-		
-		
-		
-		echo'<h3 id="directoryJobTitle">';
-		if( get_field('job_titles', 'user_' . $user->id .'') ) {
-								while ( have_rows('job_titles', 'user_' . $user->id .'') ) : the_row();
-								 $arrayJob[] = get_sub_field('job_title', 'user_' . $user->id .''); 
-								endwhile;
-								$jobTitles = implode(', ', $arrayJob);
-							
-								echo $jobTitles;
-							}
-		echo'</h3>';
-		?>
-		
-        
-        
-       
-        
-		<h4 id="directoryDepartments">
-        
-		
-		<?php the_field('department', 'user_' . $user->id .''); ?></h4>
-                        
-                        
-                        
-                        
-                        
+
+<div class="wpb_row vc_row  vc_row-fluid  mk-fullwidth-false  attched-false vc_row-fluid vc_custom_1455896967960" style="padding-bottom:20px; margin-bottom:20px; border-bottom:1px solid #ddd;">
+	<div style="" class="vc_col-sm-2 wpb_column column_container ">
+			<div class="mk-image-shortcode mk-shortcode   align-left single_line-frame inside-image " style="max-width: 600px; margin-bottom:10px">
+            	<div class="mk-image-inner">
+                	<?php
+					if( $image ) { ?>
+						<a href="<?php echo um_user_profile_url(); ?>" title="<?php echo '' . $user->display_name . ''; ?>">
+						<img src="<?php echo $thumb; ?>" alt="<?php echo '' . $user->display_name . ''; ?>" title="<?php echo '' . $user->display_name . ''; ?>" width="150px" />
+						</a>
+						 
+					<?php }
+					else { ?> 
+						<img class="lightbox-true" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" src="/wp-content/uploads/2016/01/defaul-avatar_0.jpg" itemprop="image"  width="150px" />
+					<?php } ?>  
+                </div>
+                <div class="clearboth"></div>
+            </div>
+	</div>
+	<div style="" class="vc_col-sm-6 wpb_column column_container ">
+        <h2 style="font-size: 25px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:bold;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e076fc" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style directoryNameFixer">
+            <span style="">
+                <?php		
+					echo '<a href="';
+					echo um_user_profile_url();
+					echo '" title="';
+					echo '' . $user->display_name . '';
+					echo '">';
+					echo '' . $user->display_name . '';
+					echo '</a>';
+				?>	
+            </span>
+        </h2>
+        <div class="clearboth"></div>
+        <div style=" margin-bottom:10px;text-align: left;" class="mk-text-block  true">
+            <div id="cohpa-directory-name">
+                <strong>
+                <?php		
+					if( get_field('job_titles', 'user_' . $user->id .'') ) {
+						while ( have_rows('job_titles', 'user_' . $user->id .'') ) : the_row();
+						 $arrayJob[] = get_sub_field('job_title', 'user_' . $user->id .''); 
+						endwhile;
+						$jobTitles = implode(', ', $arrayJob);
+					
+						//echo $jobTitles;
+					}
+				?>
+                </strong><br />
+                <span class="color-lightgray">
+                <?php the_field('department', 'user_' . $user->id .''); ?></h4>
                         <?php 
 						$terms = get_field('department');
 						if( $terms ) {
@@ -244,7 +122,9 @@ if ( ! empty( $user_query->results ) ) {
 						 } 	
 
 						?>
-		<?php 
+                </span><br />
+                
+                <?php 
 		if( ! empty( $user->phone_number ) ) { 
 							echo '<div id="directoryProfile-phone"><i style="color:#666;margin:4px;4px;" class="mk-moon-phone  mk-size-small"></i> Phone: ';
 							echo '' . $user->phone_number . '';
@@ -256,8 +136,23 @@ if ( ! empty( $user_query->results ) ) {
 							echo '">';
 							echo '' . $user->email_address . '';
 							echo '</a></div>';
-						}				
-						
+						}	
+						?>
+                
+               <!-- <span class="color-lightgray">Phone: </span>407-823-5884<br />
+                <span class="color-lightgray">Email: </span>
+                <a title="Contact David Janosik" href="mailto:djanosik@ucf.edu">djanosik@ucf.edu</a>-->
+                
+                
+                
+            </div>
+            <div class="clearboth"></div>
+        </div>
+        <div style="text-align: left;" class="mk-text-block  true">
+            <div id="cohpa-directory-name"></div>
+            
+            
+        <?php				
 		echo '<div id="directoryProfile-location"><i style="color:#666;margin:4px;4px;" class="mk-moon-location-4  mk-size-small"></i> Location: <a href="';
 						if ($buildingMap == 'HPA I') {echo 'http://map.ucf.edu/locations/80/health-public-affairs-i/';}
 						if ($buildingMap == 'HPA II') {echo 'http://map.ucf.edu/locations/80/health-public-affairs-ii/';}
@@ -281,12 +176,20 @@ if ( ! empty( $user_query->results ) ) {
 							echo ' Room: ';
 							echo '' . $user->room_number . '';
 						}
-						echo '</div>';	?>	
-						
-						
-						
-						
-	<?php 
+						echo '</div>';	?>
+            
+            
+            <div class="clearboth"></div>
+        </div> 
+    </div>
+    <div style="" class="vc_col-sm-4 wpb_column column_container ">
+        <h2 style="font-size: 16px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:normal;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e07f2d" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style ">
+            <span style="">Additional Info</span>
+        </h2>
+        <div class="clearboth"></div>
+        <div style="text-align: left;" class="mk-text-block  true">
+            <p>
+            <?php 
 					if(get_field('cv', 'user_' . $user->id .'') || get_field('website_url', 'user_' . $user->id .'') || get_field('facebook_url', 'user_' . $user->id .'') || get_field('linkedin_url', 'user_' . $user->id .'')) {
 					   echo '<div style=" margin-bottom:0px;text-align: left;" class="mk-text-block  "><h3 class="mk-shortcode mk-fancy-title pattern-style mk-shortcode-heading pattern-false"><span >Additional Info</span></h3>';
 					
@@ -312,7 +215,30 @@ if ( ! empty( $user_query->results ) ) {
 						}
 						
 					   echo '<p>&nbsp;</p></div>';
-					}?>					
+					}?>	
+                
+                
+                
+                
+                
+                
+            <div class="clearboth"></div>
+        </div> 
+    </div>
+</div>
+
+
+
+ <!-- END -->   
+
+
+
+
+						
+						
+						
+						
+					
 						
 	<?php 					
 						
