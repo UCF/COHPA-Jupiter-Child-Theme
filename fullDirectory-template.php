6627a6f119d7cee758c1c818b65d3bba0badc68f
cohpa <djanosik@ucf.edu>
2016-06-23 11:38:49 -0400

    tweak

--- fullDirectory-template.php
+++ fullDirectory-template.php
@@ -30,13 +30,37 @@ get_header(); ?>
 <!-- START THE CUSTOM SECTION -->
 
 <?php
-/*$args = array(
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$search = ( get_query_var( 'as' ) ) ? get_query_var( 'as' )  : '';
+
+?>
+
+<div class="author-search">
+	<h2><?php _e('Search authors by name' ,'simple-user-listing');?></h2>
+		<form method="get" id="sul-searchform" action="<?php the_permalink() ?>">
+			<label for="as" class="assistive-text"><?php _e('Search' ,'simple-user-listing');?></label>
+			<input type="text" class="field" name="as" id="sul-s" placeholder="<?php _e('Search Authors' ,'simple-user-listing');?>" value="<?php echo $search; ?>"/>
+			<input type="submit" class="submit" id="sul-searchsubmit" value="<?php _e('Search Authors' ,'simple-user-listing');?>" />
+		</form>
+	<?php
+	if( $search ){ ?>
+		<h2 ><?php printf( __('Search Results for: %s' ,'simple-user-listing'), '<em>' . $search .'</em>' );?></h2>
+		<a href="<?php the_permalink(); ?>"><?php _e('Back To Author Listing' ,'simple-user-listing');?></a>
+	<?php } ?>
+</div><!-- .author-search -->
+
+
+
+<?php
+$args = array(
 	'meta_key' => 'last_name',
     'orderby' => 'meta_value',
     'order' => 'ASC',
 	'exclude' => array(1,8,9),
 	
-);*/
+);
 
 /*$args = array(
 	'key' => 'department',
@@ -44,13 +68,13 @@ get_header(); ?>
 	'compare' => '=',
 	
 );
-*/
+
 $args = array(
 'meta_query' => array(
         'relation' => 'AND',
 			array(	
-			'meta_key' => 'email_address',
-			'meta_value' => 'amy.buford@ucf.edu',
+			'meta_key' => 'department',
+			'meta_value' => 'Social Work',
 			'meta_compare' => '=',
 			),
 			
@@ -62,7 +86,7 @@ $args = array(
 			),	
 			
     )
-);
+);*/
 
 // The Query
 $user_query = new WP_User_Query( $args );
