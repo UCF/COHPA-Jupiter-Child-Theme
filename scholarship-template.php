9274c8c5dae09ca9362da8a0f1df7d5829df2ed9
cohpa <djanosik@ucf.edu>
2016-06-29 15:26:20 -0400

    css

--- scholarship-template.php
+++ scholarship-template.php
@@ -185,8 +185,8 @@ else {
 <?php wp_reset_query(); ?> 
 																		
 <style>  
-	#list-style-568d5c104e58b ul li:before { font-family:FontAwesome !important; content: "\f00c" !important; color:#ffc904 !important;display: inline-block; width:80px;}
-	#list-style-568d5c104ed55 ul li:before { font-family:Icomoon !important; content: "\4a" !important; color:#ffc904 !important; }
+	#list-style-568d5c104e58b ul li:before { font-family:FontAwesome !important; content: "\f00c" !important; color:#ffc904 !important; display: inline-block; width:20px;}
+	#list-style-568d5c104ed55 ul li:before { font-family:Icomoon !important; content: "\4a" !important; color:#ffc904 !important; display: inline-block; width:20px; }
 	.button-568d5c104f525 { margin-bottom: 15px; margin-top: 0px; min-width: 0px !important; } 
 	.button-568d5c104f525 { background-color:#e8e8e8 !important; } 
 	.mk-button.button-568d5c104f525.flat-dimension:hover { background-color:#252525 !important; color:#ffc904 !important; } 
