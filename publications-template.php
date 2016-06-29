5c2ede0cbf8f3a28d292476a8a9186a2b64dac73
cohpa <djanosik@ucf.edu>
2016-06-29 12:39:47 -0400

    added divs

--- publications-template.php
+++ publications-template.php
@@ -17,9 +17,23 @@ $padding = ($padding == 'true') ? 'no-padding' : '';
 
 get_header(); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
+<div id="theme-page" class="master-holder clearfix" role="main" itemprop="mainContentOfPage" >
+	<div class="mk-main-wrapper-holder">
+		<div id="mk-page-id-677" class="theme-page-wrapper mk-main-wrapper mk-grid full-layout  ">
+			<div class="theme-content " itemprop="mainContentOfPage">
+            	<div class="wpb_row vc_row  mk-fullwidth-false  attched-false    vc_row-fluid  js-master-row ">
+                
+                <?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
 						<?php the_content();?>
                   <?php endwhile; ?>
                 <?php wp_reset_query(); ?>  
+
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php get_footer(); ?>
