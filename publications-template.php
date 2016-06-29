9b33c3ed599cc34d741c4b164e1f9151cddc75d3
cohpa <djanosik@ucf.edu>
2016-06-29 12:34:11 -0400

    just content

--- publications-template.php
+++ publications-template.php
@@ -17,31 +17,9 @@ $padding = ($padding == 'true') ? 'no-padding' : '';
 
 get_header(); ?>
 
-<div id="theme-page" class="master-holder clearfix" <?php echo get_schema_markup('main'); ?>>
-            <div class="mk-main-wrapper-holder">
-                <div <?php echo $wrapper_id; ?> class="theme-page-wrapper <?php
-        echo $wrapper_class; ?> <?php
-        echo $layout; ?>-layout <?php
-        echo $padding; ?> ">
-                      <div class="theme-content <?php
-        echo $padding; ?>" itemprop="mainContentOfPage">
-                            <?php
-        echo $content;
-?>                      
-                      <div class="clearboth"></div>
-                      <?php
-        if (mk_is_pages_comments_enabled()) {
-            if (comments_open()) {
-                comments_template('', true);
-            }
-        }
-?>
-                      </div>
-                <?php
-        if ($layout != 'full') { get_sidebar(); } ?>
-                <div class="clearboth"></div>
-                
-                </div>
-            </div>
+<?php if ( have_posts() ) while ( have_posts() ) : the_post();?>
+						<?php the_content();?>
+                  <?php endwhile; ?>
+                <?php wp_reset_query(); ?>  
 
 <?php get_footer(); ?>
