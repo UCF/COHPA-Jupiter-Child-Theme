9659e776a73800706636821c6a53acc72c68f0fe
cohpa <djanosik@ucf.edu>
2016-08-10 12:33:24 -0400

    phone number

--- shortcodes.php
+++ shortcodes.php
@@ -40,7 +40,7 @@ if($values)
                     </h3>
                     <p><strong>Professor</strong><br>
                         Criminal Justice<br>
-                        Phone: 407-823-3679<br>
+                        Phone: <?php echo $value['phone_number'] ; ?><br>
                         Email: <a title="Contact Kenneth Adams" href="mailto:kenneth.adams@ucf.edu">kenneth.adams@ucf.edu</a>
                     </p>
                     <p>Location: <a title="Map to " href="http://map.ucf.edu/locations/80/health-public-affairs-i/" target="_blank">HPA I</a> Room: 360</p>
