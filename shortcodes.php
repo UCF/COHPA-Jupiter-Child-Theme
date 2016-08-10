9054a94d6953425fafb8fb29e1cb2ba4222bdecd
cohpa <djanosik@ucf.edu>
2016-08-10 15:14:37 -0400

    room

--- shortcodes.php
+++ shortcodes.php
@@ -33,9 +33,9 @@ if($values)
 						echo '">';
 						the_field('building', 'user_'. $user_db );
 						echo '</a> ';
-						if( ! empty( $user->room_number ) ) { 
+						if( ! empty( $roomy ) ) { 
 							echo ' Room: ';
-							echo '' . $user->room_number . '';
+							the_field('room_number', 'user_'. $user_db );
 						}
 						echo '</div>';	?>
 
