257053ecf2b0b626031106c91580d9a6f2151b8f
cohpa <djanosik@ucf.edu>
2016-08-26 09:38:39 -0400

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -203,7 +203,9 @@ $research_ucf = get_field('research_interests', 'user_'. $user_db );
 	<div style="" class="vc_col-sm-9 wpb_column column_container  _ height-full">
 		<h2 id="fancy-title-2" class="mk-fancy-title  simple-style directoryNameFixer color-single">
 			<span>
-				<a title="View <?php echo $value['display_name'] ; ?>'s Profile" href="/directory/<?php echo strtolower(get_field('first_name', 'user_' . $user_db)); ?>-<?php echo strtolower(get_field('last_name', 'user_' . $user_db)); ?>" target="_parent"><?php echo $value['display_name'] ; ?><?php 
+				<a title="View <?php echo $value['display_name'] ; ?>'s Profile" href="/directory/<?php echo strtolower(get_field('first_name', 'user_' . $user_db)); ?>-<?php echo strtolower(get_field('last_name', 'user_' . $user_db)); ?>" target="_parent"><?php echo $value['display_name'] ; ?></a>			
+            </span>
+		</h2><?php 
 		if( get_field('degrees', 'user_'. $user_db ) ) {
 			while ( have_rows('degrees', 'user_'. $user_db ) ) : the_row();
 			 $arrayDegree[] = get_sub_field('degree', 'user_'. $user_db ); 
@@ -212,9 +214,7 @@ $research_ucf = get_field('research_interests', 'user_'. $user_db );
 
 		   echo '<span class"directoryDegrees">, ' . $degreeIDs . '</span>';
 		}
-		?></a>			
-            </span>
-		</h2>
+		?>
 		<div id="list-3" class="mk-list-styles  mk-align-none  clear" data-charcode="f00c" data-family="awesome-icons">
 		<?php 
 			$termswer = get_field('research_interests', 'user_'. $user_db );
