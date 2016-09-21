5bc55dc64429eefc611a2b4a421c5cf34dc2d65c
cohpa <djanosik@ucf.edu>
2016-09-21 10:15:32 -0400

    double icon

--- shortcodes.php
+++ shortcodes.php
@@ -93,7 +93,7 @@ $jobtitle_ucf = get_sub_field('job_title');
 						 } 	
 
 						?>
-                        <div id="directoryProfile-phone"><i style="color:#666;margin:4px 4px;" class="mk-moon-phone-4  mk-size-small"></i> Phone: <?php the_field('phone_number', 'user_'. $user_db ); ?></div>
+                        <div id="directoryProfile-phone"><i style="color:#666;margin:4px 4px;" class="mk-moon-envelop-2  mk-size-small"></i> Phone: <?php the_field('phone_number', 'user_'. $user_db ); ?></div>
                         <div id="directoryProfile-email"><i style="color:#666;margin:4px 4px;" class="mk-moon-envelop-2  mk-size-small"></i> Email: <a title="Contact <?php echo $value['display_name'] ; ?>" href="mailto:<?php the_field('email_address', 'user_'. $user_db ); ?>"><?php the_field('email_address', 'user_'. $user_db ); ?></a></div>
                     </p>
                     <?php				
