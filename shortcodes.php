5f00df18d88e4152e1497b5c14380117acffd25c
cohpa <djanosik@ucf.edu>
2017-11-13 16:59:17 -0500

    66

--- shortcodes.php
+++ shortcodes.php
@@ -615,12 +615,9 @@ function dept_option($atts){
 
 				<?php echo '<li>' . $user->display_name.' ['.$user->phone_number . ']</li>'; ?>
 
-<!-- START PUTTIN IN THE FIELDS -->  
-KNOWN WORKING SCRIPTS 88
-<div id="directoryProfile-phone"><i style="color:#666;margin:4px;4px;" class="mk-moon-phone  mk-size-small"></i> Phone: <?php echo $user->phone_number ; ?></div>
-<div id="directoryProfile-email"><i style="color:#666;margin:4px;4px;" class="mk-moon-envelop-2  mk-size-small"></i> Email: <a title="Contact <?php echo $user->display_name ; ?>" href="mailto:<?php echo $user->email_address ; ?>"><?php echo $user->email_address ; ?></a></div>
-
-<div class="wpb_column vc_column_container vc_col-sm-2">
+<!-- START REPEATER SECTION -->	
+<div class="wpb_row vc_inner vc_row    attched-false   vc_row-fluid ">
+	<div class="wpb_column vc_column_container vc_col-sm-2">
     	<div class="vc_column-inner ">
         	<div class="wpb_wrapper">
             	<div class="mk-image   align-left border_shadow-frame inside-image " style="margin-bottom:10px">
@@ -641,7 +638,97 @@ KNOWN WORKING SCRIPTS 88
             </div>
         </div>
     </div>
-<!-- END PUTTIN IN THE FIELDS -->  
+	<div class="wpb_column vc_column_container vc_col-sm-10">
+    	<div class="vc_column-inner ">
+            <div class="wpb_wrapper">
+                <div id="text-block-6" class="mk-text-block   ">
+                    <h3 style="font-weight:bold;">
+                        <a title="View <?php echo $user->display_name ; ?>'s Profile" href="/directory/<?php echo strtolower($user->first_name); ?>-<?php echo strtolower($user->last_name); ?>" target="_parent"><?php echo $user->display_name ; ?></a>
+                    </h3>
+                    <p><span style="font-weight:bold; font-size:16px;">
+                    	<?php
+						switch_to_blog(1);
+						
+							if( get_field('job_titles', 'user_' . $user_db .'') ) {
+								$num_rows = 0;
+									while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();
+									$num_rows++;
+									endwhile;
+							
+									while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();
+									$num_rows--;
+									echo '<span>'. get_sub_field('job_title') .'</span>';
+									if ( $num_rows == 0 ) { echo ''; }
+									else { echo ', '; }
+									endwhile;
+							}
+						restore_current_blog();
+							?>
+                            
+                    </span><br>
+                        <?php $user->department; ?>
+                        <?php 
+						$terms = get_field('department');
+						if( $terms ) {
+						  $count = count( $terms );
+							$i = 0;
+							$term_list = ' ';
+						foreach( $terms as $term ) {
+						  $i++;
+						  echo '<a href="';
+						  echo get_term_link( $term );
+						  echo'">';
+						  echo $term->name;
+						  echo '</a>';
+						if ( $count != $i ) {
+									echo ', ';
+								}
+							}
+						 } 	
+
+						?>
+                        <div id="directoryProfile-phone"><i style="color:#666;margin:4px;4px;" class="mk-moon-phone  mk-size-small"></i> Phone: <?php echo strtolower($user->phone_number); ?></div>
+                        <div id="directoryProfile-email"><i style="color:#666;margin:4px;4px;" class="mk-moon-envelop-2  mk-size-small"></i> Email: <a title="Contact <?php echo $user->display_name ; ?>" href="mailto:<?php echo strtolower($user->email_address); ?>"><?php echo strtolower($user->email_address); ?></a></div>
+                    </p>
+                    <?php				
+						echo '<div id="directoryProfile-location"><i style="color:#666;margin:4px;4px;" class="mk-moon-location-4  mk-size-small"></i> Location: <a href="';
+						if ($buildingMap == 'HPA I') {echo 'http://map.ucf.edu/locations/80/health-public-affairs-i/';}
+						if ($buildingMap == 'HPA II') {echo 'http://map.ucf.edu/locations/80/health-public-affairs-ii/';}
+						if ($buildingMap == 'Orlando Tech Center') {echo 'http://map.ucf.edu/locations/8113/orlando-tech-center-otc3/';}
+						if ($buildingMap == 'Research Pavilion') {echo 'http://map.ucf.edu/locations/8102/research-pavilion-pvl/';}
+						if ($buildingMap == 'UCF Cocoa') {echo 'http://map.ucf.edu/locations/cocoa/cocoa/';}
+						if ($buildingMap == 'UCF Daytona Beach') {echo 'http://map.ucf.edu/locations/daytona-beach/daytona-beach/';}
+						if ($buildingMap == 'UCF Leesburg') {echo 'http://map.ucf.edu/locations/leesburg/leesburg/';}
+						if ($buildingMap == 'UCF Ocala') {echo 'http://map.ucf.edu/locations/ocala/ocala/';}
+						if ($buildingMap == 'UCF Palm Bay') {echo 'http://map.ucf.edu/locations/palm-bay/palm-bay/';}
+						if ($buildingMap == 'UCF Sanford/Lake Mary') {echo 'http://map.ucf.edu/locations/sanford-lake-mary/sanfordlake-mary/';}
+						if ($buildingMap == 'UCF South Lake') {echo 'http://map.ucf.edu/locations/south-lake/south-lake/';}
+						if ($buildingMap == 'UCF Valencia Osceola') {echo 'http://map.ucf.edu/locations/valencia-osceola/valencia-osceola/';}
+						if ($buildingMap == 'UCF Valencia West') {echo 'http://map.ucf.edu/locations/valencia-west/valencia-west/';}
+						echo '" target="_blank" title="Map to ';
+						the_field('building', 'user_'. $user_db );
+						echo '">';
+						the_field('building', 'user_'. $user_db );
+						echo '</a> ';
+						if( ! empty( $roomy ) ) { 
+							echo ' Room: ';
+							the_field('room_number', 'user_'. $user_db );
+						}
+						echo '</div>';	?>
+    
+                    <div class="clearboth"></div>
+                </div>
+            </div>
+        </div>
+    </div>	
+</div>
+<div id="divider-7" class="mk-divider  divider_full_width center shadow_line ">
+	<div class="divider-inner">
+		<span class="divider-shadow-left"></span><span class="divider-shadow-right"></span>
+	</div>
+</div>
+<div class="clearboth"></div>
+<!-- END REPEATER SECTION -->
  <?php			}
 		echo '</ul>';
 // SEE IF THIS SHOWS UP	
