65624c6ba11c8dca0e7822c5007dc0aaf8887f77
cohpa <djanosik@ucf.edu>
2017-11-13 16:09:33 -0500

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -611,13 +611,16 @@ function dept_option($atts){
 	$subscribers = get_users($args1);
 		echo '<ul>';
  			foreach ($subscribers as $user) { 
-         		
+			 $user_db = $user['ID'];
+			 $buildingMap = get_field('building', 'user_' . $user_db .'');
+			 $roomy = get_field('room_number', 'user_' . $user_db .'');
 ?>
 
 				<?php echo '<li>' . $user->display_name.' ['.$user->phone_number . ']</li>'; ?>
-
+<?php the_field('email_address', 'user_'. $user_db ); ?>
 <!-- START PUTTIN IN THE FIELDS -->  
-<div id="directoryProfile-phone"><i style="color:#666;margin:4px;4px;" class="mk-moon-phone  mk-size-small"></i> Phone:<?php echo $user->phone_number ; ?></div>
+HERE GOES KNOW WORKING SCRIPTS
+<div id="directoryProfile-phone"><i style="color:#666;margin:4px;4px;" class="mk-moon-phone  mk-size-small"></i> Phone: <?php echo $user->phone_number ; ?></div>
 <div id="directoryProfile-email"><i style="color:#666;margin:4px;4px;" class="mk-moon-envelop-2  mk-size-small"></i> Email: <a title="Contact <?php echo $user->display_name ; ?>" href="mailto:<?php echo $user->email_address ; ?>"><?php echo $user->email_address ; ?></a></div>
 <!-- END PUTTIN IN THE FIELDS -->  
  <?php			}
