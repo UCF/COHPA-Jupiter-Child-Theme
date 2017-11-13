16f2c3d47fd7af1d0fe04ee12b27633d6a42c97d
cohpa <djanosik@ucf.edu>
2017-11-13 11:22:12 -0500

    1

--- shortcodes.php
+++ shortcodes.php
@@ -567,7 +567,7 @@ echo '<ul>';
  }
 echo '</ul>';
 ?>
-<div>is this shortcode working 4?</div>
+<div>is this shortcode working 1?</div>
 <!-- END CONTENT SECTION -->	
 	
 <style id='theme-dynamic-styles-inline-css' type='text/css'>
