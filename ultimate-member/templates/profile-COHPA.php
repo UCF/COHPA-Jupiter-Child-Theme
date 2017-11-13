84682bef638e8eb3fb6c411e47e6b80b74f16e16
cohpa <djanosik@ucf.edu>
2017-11-13 08:14:38 -0500

    new pic code

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -117,9 +117,21 @@ get_header();
     <div class="clearboth"></div>
 </div>
 
+<!-- START NEW PIC CODE -->
+<div class="mk-image  lightbox-enabled align-center border_shadow-frame inside-image " style="margin-bottom:10px">
+	<div class="mk-image-holder">
+		<div class="mk-image-inner ">
+			<img class="lightbox-true" alt="david-headshot" title="david-headshot" src="http://cohpacmsdev.smca.ucf.edu/wp-content/uploads/2016/01/david-headshot.jpg" />
+			<div class="mk-image-overlay"></div>
+			<a href="https://www.cohpa.ucf.edu/wp-content/uploads/2016/01/david-headshot.jpg" alt="david-headshot" data-fancybox-group="image-shortcode-" title="" class="mk-lightbox  mk-image-lightbox">
+				<svg  class="mk-svg-icon" data-name="mk-jupiter-icon-plus-circle" data-cacheid="icon-5a0996ea15eea" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M240 24c115.2 0 209.6 94.4 209.6 209.6s-94.4 209.6-209.6 209.6-209.6-94.4-209.6-209.6 94.4-209.6 209.6-209.6zm0-30.4c-132.8 0-240 107.2-240 240s107.2 240 240 240 240-107.2 240-240-107.2-240-240-240zm80 256h-160c-9.6 0-16-6.4-16-16s6.4-16 16-16h160c9.6 0 16 6.4 16 16s-6.4 16-16 16zm-80 80c-9.6 0-16-6.4-16-16v-160c0-9.6 6.4-16 16-16s16 6.4 16 16v160c0 9.6-6.4 16-16 16z"/></svg>
+			</a>
+		</div>
+	</div>
+	<div class="clearboth"></div>
+</div>
 
-
-
+<!--END NEW PIC CODE -->
 
 						<div class="clearboth"></div>
                     </div>
