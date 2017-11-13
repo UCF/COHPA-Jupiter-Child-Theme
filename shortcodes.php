fcc141d7e94349f0b17d31b672a4bfaea59fd49b
cohpa <djanosik@ucf.edu>
2017-11-13 09:24:46 -0500

    switch to room number

--- shortcodes.php
+++ shortcodes.php
@@ -555,7 +555,7 @@ $args1 = array(
  $subscribers = get_users($args1);
 echo '<ul>';
  foreach ($subscribers as $user) {
- echo '<li>' . $user->display_name.'['.$user->phone_number . ']</li>';
+ echo '<li>' . $user->display_name.'['.$user->room_number . ']</li>';
  }
 echo '</ul>';
 ?>
