4777d93a9133a7eefbd1e388a32a5013d59e4292
cohpa <djanosik@ucf.edu>
2017-11-14 09:51:59 -0500

    added IF

--- shortcodes.php
+++ shortcodes.php
@@ -750,9 +750,11 @@ $cohpaLNAME = preg_replace("/[\s_]/", "-", $myLNAME);
  <!-- INSERTED NEW SECTION -->            
 	<div style="" class="vc_col-sm-4 wpb_column column_container ">
 
-
+		<?php 
+		if(get_field('cv', 'user_' . $user_db .'') || get_field('website_url', 'user_' . $user_db .'') || get_field('facebook_url', 'user_' . $user_db .'') || get_field('linkedin_url', 'user_' . $user_db .'')) {
+		   ?>
 		<h2 style="font-size: 16px;text-align:left;color: #3d3d3d;font-style:inherit;font-weight:normal;padding-top:0px;padding-bottom:0px; text-transform:initial;letter-spacing:0px;" id="fancy-title-5762ba5e07f2d" class="mk-shortcode mk-fancy-title fancy-title-align-left simple-style "><span style="">Additional Info</span></h2>
-
+		<?php  }?>	
 		<div class="clearboth"></div>
 		<div style="text-align: left;" class="mk-text-block  true">
 			<p>
