9c5298604e98c2c0e2768f24d093bdb236fcdbd8
cohpa <djanosik@ucf.edu>
2018-01-09 11:32:17 -0500

    revert

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -281,7 +281,7 @@ $( "ul.mk-tabs-tabs li" ).first().addClass( "lookhere" );
 </script>		
 
 <style>
-	#mk-tabs-2 li:first-child {
+	#mk-tabs-2 .mk-tabs-tabs .is-active a {
     background-color: red !important;
 }
 </style>
