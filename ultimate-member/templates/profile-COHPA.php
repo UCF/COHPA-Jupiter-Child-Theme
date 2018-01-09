6a7741e238f6da196a13aa4205a80393a2f92dff
cohpa <djanosik@ucf.edu>
2018-01-09 11:25:31 -0500

    all tabs

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -281,7 +281,7 @@ $( "ul.mk-tabs-tabs li" ).first().addClass( "lookhere" );
 </script>		
 
 <style>
-	#mk-tabs-2 .mk-tabs-tabs a {
+	#mk-tabs-2 .mk-tabs-tabs li.mk-tabs-tab {
     background-color: red !important;
 }
 </style>
