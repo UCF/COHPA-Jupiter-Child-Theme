bfe6ef1ac7e4f173068d9d76f77d554a22f362fc
cohpa <djanosik@ucf.edu>
2018-01-09 11:34:15 -0500

    first child

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -281,7 +281,7 @@ $( "ul.mk-tabs-tabs li" ).first().addClass( "lookhere" );
 </script>		
 
 <style>
-	#mk-tabs-2 .mk-tabs-tabs li a {
+	#mk-tabs-2 .mk-tabs-tabs li a:first-child {
     background-color: red !important;
 }
 </style>
