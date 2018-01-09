e62f71aaf54c33475c6a14406d21c04de5730ea0
cohpa <djanosik@ucf.edu>
2018-01-09 11:23:07 -0500

    new css

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -281,7 +281,7 @@ $( "ul.mk-tabs-tabs li" ).first().addClass( "lookhere" );
 </script>		
 
 <style>
-	#mk-tabs-2 .mk-tabs-tabs .is-active a, #mk-tabs-2 .mk-fancy-title span {
+	#mk-tabs-2 .mk-tabs-tabs .is-active a {
     background-color: red !important;
 }
 </style>
