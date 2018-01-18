6a72c8a8b6a5fb57d2793230df13389a15c80e58
cohpa <djanosik@ucf.edu>
2018-01-18 10:14:27 -0500

    revert

--- ultimate-member/templates/profile-COHPA.php
+++ ultimate-member/templates/profile-COHPA.php
@@ -171,7 +171,7 @@ get_header();
 							}
 						 }
 						?>
-		<div id="new-directoryIcons" data-charcode="mk-icon-phone" class="mk-align-none   clear" data-family=""><ul>
+		<div id="new-directoryIcons" data-charcode="mk-icon-phone" class="mk-list-styles  mk-align-none   clear" data-family=""><ul>
 						<?php if(get_field('phone_number', 'user_' . $user_id .'')) { 
 							echo '<li><svg  class="mk-svg-icon" data-name="mk-moon-phone" data-cacheid="icon-5a553bfdc3fd3" style=" height:16px; width: 16px; "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M352 320c-32 32-32 64-64 64s-64-32-96-64-64-64-64-96l64-64s-64-128-96-128l-96 96c0 64 65.75 193.75 128 256s192 128 256 128c0 0 96-64 96-96s-96-128-128-96z"/></svg>Phone: ';
 							the_field('phone_number', 'user_' . $user_id .'');
