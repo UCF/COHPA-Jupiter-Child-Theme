2366764cfee2f6a924b8f43be0ed1b804df6f73c
cohpa <djanosik@ucf.edu>
2018-06-15 11:01:11 -0400

    looking for 1

--- shortcodes.php
+++ shortcodes.php
@@ -77,21 +77,22 @@ $imageBackup = bfi_thumb( "/wp-content/uploads/2016/01/defaul-avatar_0.jpg", $pa
 .mk-divider.shadow_line .divider-inner .divider-shadow-left { background-position:left center; } 
 .mk-divider.shadow_line .divider-inner .divider-shadow-right { background-position:right center; }
 .mk-image.border_shadow-frame .mk-image-inner { border:6px solid #ffffff; box-shadow:0 0 5px rgba(0, 0, 0, 0.25); } 
-</style>	
-<?php });// END SHORTCODE [show_research] ?> <?php add_shortcode('show_announcements', function() { ?><!-- START REPEATER SECTION -->	
+</style> <?php });// END SHORTCODE [show_research] ?> <?php add_shortcode('show_UCFannouncements', function() {
+$myfavetools = new WP_Query(array(
+								'post_type'	=> 'announcements',
+								'posts_per_page' => '1',
+								'order'=>'DESC'
+							)); ?> <?php while($myfavetools->have_posts()) : $myfavetools->the_post(); ?><!-- START THE REPEAT SECTION -->  
+<div id="announ"><span class="date"><?php echo get_the_date(); ?></span><br /><?php the_title( '<strong>', '</strong>' ); ?><br /><?php the_content();?>
+</div><!-- END OF THE REPEAT SECTION --><?php endwhile; ?><?php wp_reset_query(); ?> <a href="<?php echo get_site_url(); ?>/announcements/" title="View All Accouncements">View All</a>
+<?php }); // END SHORTCODE [show_UCFannouncements]?> <?php add_shortcode('show_announcements', function() { ?><!-- START REPEATER SECTION -->	
 <?php $myannouncements = new WP_Query(array(
 								'post_type'	=> 'announcement',
 								'orderby'=>'title',
 								'order'=>'ASC'
 							)); ?> <?php while($myannouncements->have_posts()) : $myannouncements->the_post(); ?> 				<!--START OF THE REPEAT SECTION -->
                 <li style="list-style:none; font-size:16px !important;"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
- <div style="margin-left:25px;">               
-                <?php $repeater = get_field('announcement_items');
-$announdate = get_sub_field('announcement_date');
-$last_row = end($repeater);
-echo $last_row['announcement_date'];
-echo $last_row['announcement_item'];
-?> </div> <!-- END OF THE REPEAT SECTION --> <?php endwhile; ?> <!-- END OF THE REPEAT SECTION -->	<?php	});// END SHORTCODE [show_announcements]?><?php /**Shortcode: [deptlist department=""] */ function dept_option($atts){
+ <div style="margin-left:25px;"> <?php $repeater = get_field('announcement_items'); $announdate = get_sub_field('announcement_date'); $last_row = end($repeater); echo $last_row['announcement_date']; echo $last_row['announcement_item']; ?> </div> <!-- END OF THE REPEAT SECTION --> <?php endwhile; ?> <!-- END OF THE REPEAT SECTION -->	<?php	});// END SHORTCODE [show_announcements]?><?php /**Shortcode: [deptlist department=""] */ function dept_option($atts){
 	extract(shortcode_atts( array(
 		'department' => 'Legal Studies',
 	), $atts ));
