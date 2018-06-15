395df34515a17eb0f76283c465e5b96f0978c448
cohpa <djanosik@ucf.edu>
2018-06-15 10:22:55 -0400

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -1,6 +1,5 @@
 <?php add_shortcode('show_faculty', function() { $values = get_field('choose_directory'); if($values) { 	foreach($values as $value)	{  $user_db = $value['ID']; $buildingMap = get_field('building', 'user_' . $user_db .''); $roomy = get_field('room_number', 'user_' . $user_db .''); switch_to_blog(1); $image_ucf = get_field('upload_headshot', 'user_' . $user_db .''); $jobs_ucf = get_field('job_titles', 'user_' . $user_db .''); $jobtitle_ucf = get_sub_field('job_title'); restore_current_blog(); $myFNAMEDirectory = strtolower(get_field('first_name', 'user_' . $user_db));
-$myFNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myFNAMEDirectory); $cohpaFNAME = preg_replace("/[\s_]/", "-", $myFNAME); $myLNAMEDirectory = strtolower(get_field('last_name', 'user_' . $user_db)); $myLNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myLNAMEDirectory); $cohpaLNAME = preg_replace("/[\s_]/", "-", $myLNAME); ?><!-- START REPEATER SECTION -->	
-<div class="wpb_row vc_inner vc_row    attched-false   vc_row-fluid ">
+$myFNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myFNAMEDirectory); $cohpaFNAME = preg_replace("/[\s_]/", "-", $myFNAME); $myLNAMEDirectory = strtolower(get_field('last_name', 'user_' . $user_db)); $myLNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myLNAMEDirectory); $cohpaLNAME = preg_replace("/[\s_]/", "-", $myLNAME); ?><!-- START REPEATER SECTION -->	<div class="wpb_row vc_inner vc_row    attched-false   vc_row-fluid ">
 	<div class="wpb_column vc_column_container vc_col-sm-2">
     	<div class="vc_column-inner ">
         	<div class="wpb_wrapper">
@@ -24,30 +23,8 @@ $myFNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myFNAMEDirectory); $cohpaFNAME =
                     <h3 style="font-weight:bold;">
                         <a title="View <?php echo $value['display_name'] ; ?>'s Profile" href="/directory/<?php echo $cohpaFNAME; ?>-<?php echo $cohpaLNAME; ?>" target="_parent"><?php echo $value['display_name'] ; ?></a>
                     </h3>
-                    <p><div id="showFacultyJobTitle">
-                    	<?php switch_to_blog(1); if( get_field('job_titles', 'user_' . $user_db .'') ) { $num_rows = 0;
-									while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();
-									$num_rows++; endwhile;
-									while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();
-									$num_rows--; echo '<span>'. get_sub_field('job_title') .'</span>';
-									if ( $num_rows == 0 ) { echo ''; }
-									else { echo ', '; }
-									endwhile;
-							}	restore_current_blog(); ?> </div>
-                    <div id="showFacultyDepartment">
-                        <?php the_field('department', 'user_' . $user_db);  
-						$terms = get_field('department');
-						if( $terms ) { $count = count( $terms );
-							$i = 0;
-							$term_list = ' ';
-						foreach( $terms as $term ) {
-						  $i++;
-						  echo '<a href="';
-						  echo get_term_link( $term );
-						  echo'">';
-						  echo $term->name;
-						  echo '</a>';
-						if ( $count != $i ) { echo ', '; } } } ?></div>
+                    <p><div id="showFacultyJobTitle">  <?php switch_to_blog(1); if( get_field('job_titles', 'user_' . $user_db .'') ) { $num_rows = 0;	while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();	$num_rows++; endwhile; 	while ( have_rows('job_titles', 'user_' . $user_db .'') ) : the_row();	$num_rows--; echo '<span>'. get_sub_field('job_title') .'</span>';	if ( $num_rows == 0 ) { echo ''; }	else { echo ', '; }		endwhile; }	restore_current_blog(); ?> </div>
+                    <div id="showFacultyDepartment"> <?php the_field('department', 'user_' . $user_db);  $terms = get_field('department');	if( $terms ) { $count = count( $terms ); $i = 0;	$term_list = ' ';	foreach( $terms as $term ) {  $i++;	  echo '<a href="';	  echo get_term_link( $term );  echo'">';  echo $term->name;  echo '</a>';	if ( $count != $i ) { echo ', '; } } } ?></div>
                         <div id="directoryProfile-phone"><i style="color:#666;margin:4px;" class="mk-moon-phone  mk-size-small"></i> Phone: <?php the_field('phone_number', 'user_'. $user_db ); ?></div>
                         <div id="directoryProfile-email"><i style="color:#666;margin:4px;" class="mk-moon-envelop-2  mk-size-small"></i> Email: <a title="Contact <?php echo $value['display_name'] ; ?>" href="mailto:<?php the_field('email_address', 'user_'. $user_db ); ?>"><?php the_field('email_address', 'user_'. $user_db ); ?></a></div>
                     </p><?php echo '<div id="directoryProfile-location"><i style="color:#666;margin:4px;4px;" class="mk-moon-location-4  mk-size-small"></i> Location: <a href="';
@@ -64,15 +41,7 @@ $myFNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myFNAMEDirectory); $cohpaFNAME =
 						if ($buildingMap == 'UCF South Lake') {echo 'http://map.ucf.edu/locations/south-lake/south-lake/';}
 						if ($buildingMap == 'UCF Valencia Osceola') {echo 'http://map.ucf.edu/locations/valencia-osceola/valencia-osceola/';}
 						if ($buildingMap == 'UCF Valencia West') {echo 'http://map.ucf.edu/locations/valencia-west/valencia-west/';}
-						echo '" target="_blank" title="Map to ';
-						the_field('building', 'user_'. $user_db );
-						echo '">';
-						the_field('building', 'user_'. $user_db );
-						echo '</a> ';
-						if( ! empty( $roomy ) ) { 
-							echo ' Room: ';
-							the_field('room_number', 'user_'. $user_db );
-						} echo '</div>';	?> <div class="clearboth"></div>
+						echo '" target="_blank" title="Map to '; the_field('building', 'user_'. $user_db );	echo '">';	the_field('building', 'user_'. $user_db );	echo '</a> ';	if( ! empty( $roomy ) ) { 	echo ' Room: ';	the_field('room_number', 'user_'. $user_db ); } echo '</div>';	?> <div class="clearboth"></div>
                 </div>
             </div>
         </div>
@@ -83,8 +52,7 @@ $myFNAME = preg_replace("/[^a-z0-9_\s-]/", "", $myFNAMEDirectory); $cohpaFNAME =
 		<span class="divider-shadow-left"></span><span class="divider-shadow-right"></span>
 	</div>
 </div>
-<div class="clearboth"></div><!-- END REPEATER SECTION -->	
-<?php	}}		?> <style id='theme-dynamic-styles-inline-css' type='text/css'> #divider-7 { padding:10px 0 30px; } 
+<div class="clearboth"></div><!-- END REPEATER SECTION -->	<?php	}}		?> <style id='theme-dynamic-styles-inline-css' type='text/css'> #divider-7 { padding:10px 0 30px; } 
 #divider-7 .divider-inner { } #divider-7 .divider-inner:after { } #divider-7 .divider-shadow-left, #divider-7 .divider-shadow-right { background-image:url(/wp-content/themes/jupiter/assets/images/shadow-divider.png); } 
 .mk-divider.shadow_line .divider-inner { height:7px; } 
 .mk-divider.shadow_line .divider-inner .divider-shadow-left, .mk-divider.shadow_line .divider-inner .divider-shadow-right { display:inline-block; width:50%; height:7px; background-repeat:no-repeat; } 
